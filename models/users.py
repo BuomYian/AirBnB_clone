cbd3213aa9df02d272a42ec39b231db0e297df2c
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-11-26 11:27:39 +0300

    added documentations

--- models/users.py
+++ models/users.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
+"""
+a class User that inherits from BaseModel:
+"""
+import models
 from models.base_model import BaseModel
 
-class Class(BaseModel):
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+
+class User(BaseModel):
+    """
+    ------------------
+    CLASS: User
+    DESCRIPTION:
+             a class User that inherits from BaseModel
+    ------------------
+    """
+
+    # ------------------------------- #
+    #       PUBLIC ATTRIBUTES         #
+    # ------------------------------- #
+
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
