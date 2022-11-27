bb27ff8706d8b54dc1686c22471714b4ae99a7c9
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-11-27 10:35:39 +0300

    Delete models directory

--- models/amenity.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Amenity class that inherits from BaseModel"""
-from models.base_model import BaseModel
-
-
-class Amenity:
-    """Atributtes for the Amenity class"""
-    name = ""
--- models/state.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""State class that inherit from BaseModel"""
-from models.base_model import BaseModel
-
-
-class State(BaseModel):
-    """Attribute for name of the state"""
-    name = ""
